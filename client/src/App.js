1cf8858bb4773258e31d332d8d0236694db477cc
enajera1 <eriknajera013@gmail.com>
2022-08-07 09:58:03 -0500

    authentication added. profile expirience updated

--- client/src/App.js
+++ client/src/App.js
@@ -13,12 +13,24 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+import { setContext } from '@apollo/client/link/context';
+
 const httpLink = createHttpLink({
   uri: '/graphql', // Uniform Resource Identifier
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
